ac747d59e8ac87cf183e6d2ff0f8bf24f0cbb93c
vigneshE1999 <vigneshdevops199@gmail.com>
2025-04-04 22:38:57 +0530

    update

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 const resumeLists = document.querySelectorAll('.resume-list');
 const resumeBoxs = document.querySelectorAll('.resume-box');
 
-
-
+const portfolioLists = document.querySelectorAll('.portfolio-list');
+const portfolioBoxs = document.querySelectorAll('.portfolio-box');
 
 resumeLists.forEach((list, idx) => {
     list.addEventListener('click', () => {
@@ -11,5 +11,15 @@ resumeLists.forEach((list, idx) => {
 
         document.querySelector('.resume-box.active').classList.remove('active');
         resumeBoxs[idx].classList.add('active');
+    });
+});
+
+portfolioLists.forEach((list, idx) => {
+    list.addEventListener('click', () => {
+        document.querySelector('.portfolio-list.active').classList.remove('active');
+        list.classList.add('active');
+
+        document.querySelector('.portfolio-box.active').classList.remove('active');
+        portfolioBoxs[idx].classList.add('active');
     });
 });
